cd1b48b23c11c29f2c588068d0c7c0d7544f96c9
Sandy <sandycchen30@gmail.com>
2022-04-26 22:38:32 -0700

    integrated front end

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 const res = require('express/lib/response');
 const fs = require('fs');
 const path = require('path');
@@ -8,9 +9,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
-
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -98,6 +99,22 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
